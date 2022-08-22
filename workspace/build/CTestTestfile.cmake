074876bb7e047870c0d8ff81f2eab65eb7e40f87
OGUNLOLU DANIEL <amazing.comptech@gmail.com>
2022-08-22 10:26:57 +0100

    Delete workspace directory

--- workspace/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/danie/Desktop/projects/ROS-PID/workspace/src
-# Build directory: /mnt/c/Users/danie/Desktop/projects/ROS-PID/workspace/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("pid")
--- workspace/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/mnt/c/Users/danie/Desktop/projects/ROS-PID/workspace/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/mnt/c/Users/danie/Desktop/projects/ROS-PID/workspace/devel;/opt/ros/noetic'.split(';') if '/mnt/c/Users/danie/Desktop/projects/ROS-PID/workspace/devel;/opt/ros/noetic' != '' else []
